0455f8cfccc1f5b694d04eadd6e8d0296c64194f
aishvary000 <19ucc070@lnmiit.ac.in>
2022-04-15 23:10:46 +0530

    awie

--- Backened/src/app.js
+++ Backened/src/app.js
@@ -101,7 +101,7 @@ app.post("/userRegister", async (req, res) => {
 app.post("/login", passport.authenticate('local',{
   
   successRedirect:'/',
-  failureRedirect:'/login.html',
+  failureRedirect:'pages/login.ejs',
   failureFlash:true
 }))
 
